96578e57dbc5339bc6858126ad68d500f230269d
Lucas LELIEVRE <llelievr@student.42.fr>
2019-03-18 19:58:49 +0100

    Je vous emmerde et je rentre a ma maison !

--- srcs/printf/specifiers/spe_f.c
+++ srcs/printf/specifiers/spe_f.c
@@ -6,40 +6,81 @@
 /*   By: llelievr <llelievr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/15 11:01:06 by llelievr          #+#    #+#             */
-/*   Updated: 2019/03/15 19:09:01 by llelievr         ###   ########.fr       */
+/*   Updated: 2019/03/18 18:21:23 by llelievr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-long double		abs_ld(long double a)
+long double	abs_ld(long double a)
 {
 	return (a < 0 ? -a : a);
 }
 
-int		specifier_f(t_arg i, t_printf *inst, t_format *f)
+long double	powld(long double n, int pow)
 {
-	/*const intmax_t		ipart = (intmax_t)i.d;
-	t_int_str	str;
-	char	c;
+	long double	out;
 
-	str = ft_uint_to_str(abs_uintmaxt(i.d));
-	write_buf(inst, str.str, str.len);
+	if (pow == 1)
+		return (n);
+	if (pow == 0)
+		return (1);
+	out = 1;
+	if (pow < 0)
+		n = 1 / n;
+	pow = ft_abs(pow);
+	while (--pow >= 0)
+		out *= n;
+	return (out);
+}
+
+void		computeld(t_format *f, t_int_str *s_ipart, t_int_str *s_dpart,
+	long double d)
+{
+	uintmax_t	ipart;
+	uintmax_t	dpart;
+
+	d += (5. * powld(10, -f->precision - 1)) * (d < 0 ? -1. : 1.);
+	ipart = abs_uintmaxt(d);
+	dpart = (long double)(abs_ld(d) - ipart) * powld(10, f->precision);
+	*s_ipart = ft_uint_to_str(ipart);
+	*s_dpart = ft_uint_to_str(dpart);
+}
+
+void		writeld(t_printf *inst, t_format *f,
+	t_int_str *s_ipart, t_int_str *s_dpart)
+{
+	write_buf(inst, s_ipart->str, s_ipart->len);
+	if (f->precision == 0 && !(f->flags & f_hash))
+		return ;
 	write_buf(inst, ".", 1);
+	if ((f->precision <= 1 && s_dpart->len == 1 && s_dpart->str[0] == '0') || f->precision == 0)
+		return ;
+	write_buf(inst, s_dpart->str, s_dpart->len);
+	if (f->precision > s_dpart->len)
+		repeat_char(inst, '0', f->precision - s_dpart->len);
+}
 
-	intmax_t mut_ab = ft_pow(10, 12);
-	intmax_t after_point_flt = (intmax_t)(i.d * mut_ab);
-	intmax_t multiplier = ft_pow(10, (12 - 1));
-	for(int xy=0; xy<12; xy++)
-    {
-        intmax_t muted = (after_point_flt / multiplier);
-		//printf("%d\n",muted);
-        muted = muted % 10;
-		c = ((muted) + '0');
-        write_buf(inst, &c, 1);
-        multiplier /= 10;
-    }*/
-    uintmax_t d = (long double)(abs_ld(i.d) - abs_uintmaxt(i.d)) * ft_pow(10, f->precision);
-    printf ("%d %d\n", abs_uintmaxt(i.d), d);
-    return (0);
+int			specifier_f(t_arg i, t_printf *inst, t_format *f)
+{
+	t_int_str	s_ipart;
+	t_int_str	s_dpart;
+	uintmax_t	len;
+
+	if (f->precision == -1)
+		f->precision = 6;
+	computeld(f, &s_ipart, &s_dpart, i.d);
+	len = s_ipart.len + 1 + s_dpart.len + (f->precision - s_dpart.len) + (!!(f->flags & f_plus) || i.d < 0) + !!(f->flags & f_space);
+	if (f->width > len && !(f->flags & f_minus) && !(f->flags & f_zero))
+		repeat_char(inst, ' ', f->width - len);
+	if (f->flags & f_space)
+		write_buf(inst, " ", 1);
+	if (i.d < 0 || f->flags & f_plus)
+		write_buf(inst, i.d < 0 ? "-" : "+", 1);
+	if (f->width > len && !(f->flags & f_minus) && !!(f->flags & f_zero))
+		repeat_char(inst, '0', f->width - len);
+	writeld(inst, f, &s_ipart, &s_dpart);
+	if (f->width > len && !!(f->flags & f_minus))
+		repeat_char(inst, ' ', f->width - len);
+	return (0);
 }
--- test.c
+++ test.c
@@ -11,6 +11,43 @@
 
 int		main(void)
 {
-	int i = 24;
-	TEST("je suis une licorne %.7f %i\n", -10.99999971, i);
+/*	int i = 24;
+	TEST("je suis une licorne %+20f %i\n", 10.99989971, i);*/
+		double		nb;
+	nb = -12547.58;
+
+    TEST("space:% f\n", nb);
+    TEST("plus:%+f\n", nb);
+    TEST("hash:%#f\n", nb);
+    TEST("precision:%.2f\n", nb);
+    TEST("big prec:%.14f\n", nb);
+    TEST("precision + hash:%#.0f\n", nb);
+    TEST("space + prec:% .5f\n", nb);
+    TEST("space + prec + hash:%# .0f\n", nb);
+    TEST("space + prec + hash:% #.0f\n", nb);
+    TEST("Plus + prec / grande:%+.5f\n", nb);
+    TEST("Plus + prec / petite:%+.0f\n", nb);
+    TEST("Plus + prec + hash:%#+.0f\n", nb);
+    TEST("Prec + 0:%0.5f\n", nb);
+    TEST("Prec + minus:%-.5f\n", nb);
+    TEST("size:%5f\n", nb);
+    TEST("size + space:% 5f\n", nb);
+    TEST("size + plus:%+5f\n", nb);
+    TEST("size + space:%# 5f\n", nb);
+    TEST("size + plus:%#+5f\n", nb);
+    TEST("size + minus:%-5f\n", nb);
+    TEST("size + 0:%05f\n", nb);
+    TEST("size + 0 + plus:%+05f\n", nb);
+    TEST("size + 0 + plus:%0+5f\n", nb);
+    TEST("size + 0 + prec:%05.3f\n", nb);
+    TEST("size + 0 + prec + hash:%0#5.0f\n", nb);
+    TEST("size + minus + prec:%-5.3f\n", nb);
+    TEST("size + minus + prec + hash:%-#5.0f\n", nb);
+    TEST("size + plus + 0 + prec:%+05.3f\n", nb);
+    TEST("size + plus + 0 + prec + hash:%0+#5.0f\n", nb);
+    TEST("size + espace + zero + prec:%0 5.3f\n", nb);
+    TEST("size + espace + zero + prec:% 05.3f\n", nb);
+    TEST("size + espace + zero + prec + hash:%#0 5.0f\n", nb);
+    TEST("size + minus + plus + prec:%-+5.3f\n", nb);
+    TEST("size + minus + plus + prec + hash:%-#+5.0f\n", nb);
 }
